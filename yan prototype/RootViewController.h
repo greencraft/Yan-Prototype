e4eb173045c0ca8bdf4fa076ea418981c4dfea4b
Steve Han <stevehan@Steve-Hans-MacBook-Pro.local>
2011-08-02 16:20:10 -0700

    Initial Commit

---
+++ yan prototype/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  yan prototype
+//
+//  Created by Steve Han on 8/2/11.
+//  Copyright ucsd 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
